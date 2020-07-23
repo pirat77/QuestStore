fb61a737f19f3563eac9d60a09be1c5c91d2759e
Jasio <mcyanek@gmail.com>
2020-07-23 14:40:49 +0200

    implements classroom dao

--- src/main/java/DAO/ClassroomDAO.java
+++ src/main/java/DAO/ClassroomDAO.java
@@ -1,4 +1,49 @@
 package DAO;
+import SQL.SQLDao;
+import model.groups.Classroom;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ClassroomDAO {
+public class ClassroomDAO  extends SQLDao<Classroom> implements Dao<Classroom> {
+
+    ClassroomDAO(){
+        super("classrooms", new String[]{});
+    }
+
+    @Override
+    protected String[] objectToArray(Classroom classroom) {
+        String id = Integer.toString(classroom.getId());
+        String name = classroom.getName();
+        return new String[]{id, name};
+    }
+
+    @Override
+    public void update(Classroom classroom) {
+        updateRecord(objectToArray(classroom));
+    }
+
+    @Override
+    public void remove(Classroom classroom) { removeRecord(Integer.toString(classroom.getId())); }
+
+    @Override
+    public void insert(Classroom classroom) { insertRecord(objectToArray(classroom)); }
+
+    @Override
+    public List<Classroom> getObjects(String columnName, String columnValue) {
+        List<Classroom> classrooms = new ArrayList<>();
+        ResultSet resultSet = getRecords(columnName, columnValue);
+        try {
+            while (resultSet.next()) {
+                Classroom classroom = new Classroom();
+                classroom.setId(resultSet.getInt("id"));
+                classroom.setName(resultSet.getString("name"));
+                classrooms.add(classroom);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return classrooms;
+    }
 }
