b2875134a3c12904346d6717c4e2006acd31290e
Jasio <mcyanek@gmail.com>
2020-07-23 14:51:22 +0200

    implements artifact dao

--- src/main/java/DAO/ArtifactDAO.java
+++ src/main/java/DAO/ArtifactDAO.java
@@ -1,4 +1,57 @@
 package DAO;
 
-public class ArtifactDAO {
+import SQL.SQLDao;
+import model.elements.Artifact;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArtifactDAO  extends SQLDao<Artifact> implements Dao<Artifact> {
+
+    ArtifactDAO(){
+        super("artifacts", new String[]{});
+    }
+
+    @Override
+    protected String[] objectToArray(Artifact artifact) {
+        String id = Integer.toString(artifact.getId());
+        String name =  artifact.getName();
+        String description = artifact.getDescription();
+        String value = Integer.toString(artifact.getValue());
+        String category_id = Integer.toString(artifact.getCategoryId());
+        return new String[]{id, name, description, value, description, value, category_id};
+    }
+
+    @Override
+    public void update(Artifact artifact) {
+        updateRecord(objectToArray(artifact));
+    }
+
+    @Override
+    public void remove(Artifact artifact) { removeRecord(Integer.toString(artifact.getId())); }
+
+    @Override
+    public void insert(Artifact artifact) { insertRecord(objectToArray(artifact)); }
+
+    @Override
+    public List<Artifact> getObjects(String columnName, String columnValue) {
+        List<Artifact> artifacts = new ArrayList<>();
+        ResultSet resultSet = getRecords(columnName, columnValue);
+        try {
+            while (resultSet.next()) {
+                Artifact artifact = new Artifact();
+                artifact.setId(resultSet.getInt("id"));
+                artifact.setName(resultSet.getString("name"));
+                artifact.setDescription(resultSet.getString("description"));
+                artifact.setValue(resultSet.getInt("value"));
+                artifact.setCategoryId(resultSet.getInt("category_id"));
+                artifacts.add(artifact);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return artifacts;
+    }
 }
