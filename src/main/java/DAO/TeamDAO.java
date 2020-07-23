769ae4a03112decb2939bfd2c8751e62100301d5
Jasio <mcyanek@gmail.com>
2020-07-23 14:34:23 +0200

    implemnts team dao

--- src/main/java/DAO/TeamDAO.java
+++ src/main/java/DAO/TeamDAO.java
@@ -1,4 +1,49 @@
 package DAO;
 
-public class TeamDAO {
+import SQL.SQLDao;
+import model.groups.Team;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TeamDAO  extends SQLDao<Team> implements Dao<Team> {
+
+    TeamDAO(){
+        super("teams", new String[]{"id", "name"});
+    }
+
+    @Override
+    protected String[] objectToArray(Team team) {
+        String id = Integer.toString(team.getId());
+        String name = team.getName();
+        return new String[]{id, name};
+    }
+
+    @Override
+    public void update(Team team) { updateRecord(objectToArray(team)); }
+
+    @Override
+    public void remove(Team team) { removeRecord(Integer.toString(team.getId())); }
+
+    @Override
+    public void insert(Team team) { insertRecord(objectToArray(team)); }
+
+    @Override
+    public List<Team> getObjects(String columnName, String columnValue) {
+        List<Team> teams = new ArrayList<>();
+        ResultSet resultSet = getRecords(columnName, columnValue);
+        try {
+            while (resultSet.next()) {
+                Team team = new Team();
+                team.setId(resultSet.getInt("id"));
+                team.setName(resultSet.getString("name"));
+                teams.add(team);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return teams;
+    }
 }
