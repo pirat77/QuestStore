904e0ad167b8ad0bfb936d4c04c2688773000c7b
Jasio <mcyanek@gmail.com>
2020-08-18 15:55:56 +0200

    implements services for artifact and quest dao, allowing jtwig to be feeded by database contents (css removed for easier testing temporarly)

--- src/main/java/service/QuestService.java
+++ src/main/java/service/QuestService.java
@@ -1,4 +1,26 @@
 package service;
 
+import DAO.QuestDAO;
+import model.elements.Quest;
+
+import java.util.List;
+
 public class QuestService {
+    QuestDAO questDao;
+    static QuestService questServiceInstance;
+
+    QuestService(){
+        this.questDao = new QuestDAO();
+    }
+
+    static public  QuestService getInstance(){
+        if (questServiceInstance != null) return questServiceInstance;
+        else questServiceInstance = new QuestService();
+        return questServiceInstance;
+    }
+
+    public List<Quest> getAllQuest(){
+        return this.questDao.getObjects("id", "%");
+    }
+
 }
