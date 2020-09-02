2673343ac2783a260a5fd01c54765722092333f8
pietia <piotrstepak@gmail.com>
2020-09-02 13:48:48 +0200

    Override equals and hashCode methods

--- src/main/java/model/Entry.java
+++ src/main/java/model/Entry.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 public class Entry {
     private String columnName;
     private String columnValue;
@@ -27,5 +29,19 @@ public class Entry {
 
     public void setColumnValue(String columnValue) {
         this.columnValue = columnValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Entry)) return false;
+        Entry entry = (Entry) o;
+        return Objects.equals(getColumnName(), entry.getColumnName()) &&
+                Objects.equals(getColumnValue(), entry.getColumnValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getColumnName(), getColumnValue());
     }
 }
