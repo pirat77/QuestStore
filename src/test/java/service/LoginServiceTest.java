19a9e74a617ffcd1ba1347f22dbc4c69c9a7630d
pietia <piotrstepak@gmail.com>
2020-09-02 16:33:48 +0200

    Tests are working

--- src/test/java/service/LoginServiceTest.java
+++ src/test/java/service/LoginServiceTest.java
@@ -24,10 +24,8 @@ class LoginServiceTest {
     private String password = "jasio";
     private String login = "jasio";
     private LoginService loginService;
-//    @Mock
     private Entry properEntry;
     private Entry invalidEntry;
-//    @Mock
     private User user;
 
     @Mock
@@ -35,18 +33,8 @@ class LoginServiceTest {
 
     @BeforeEach
     public void setUp() {
-//        mockUserDAO = new UserDAO();
-//        mockUserDAO = mock(UserDAO.class);
         properEntry = new Entry("login","jasio");
         invalidEntry = new Entry("login","invalidLogin");
-//        mockEntry = mock(Entry.class);
-//        when(mockEntry.getColumnValue()).thenReturn("login");
-//        when(mockEntry.getColumnName()).thenReturn("jasio");
-//        mockEntry.setColumnValue("login");
-//        mockEntry.setColumnName("jasio");
-
-//        mockUser = new User();
-//        mockUser = mock(User.class);
         user = new User();
         user.setId(1);
         user.setLogin("jasio");
@@ -56,17 +44,16 @@ class LoginServiceTest {
         user.setStudentId(1);
         user.setActive(true);
         user.setUserTypeId(3);
-
-        List<User> listWithExpectedUser = Collections.singletonList(user);
-        when(mockUserDAO.getObjects(properEntry)).thenReturn(listWithExpectedUser);
+//        List<User> listWithExpectedUser = Collections.singletonList(user);
+//        when(mockUserDAO.getObjects(properEntry)).thenReturn(listWithExpectedUser);
 //        when(mockUserDAO.getObjects(any()).thenReturn(Collections.emptyList()));
-//        when(mockUserDAO.getObjects(invalidEntryLogin)).thenReturn(Collections.emptyList());
-//        loginService = new LoginService(mockUserDAO);
         loginService = LoginService.getTestInstance(mockUserDAO);
     }
 
     @Test
     public void testShouldReturnUser() {
+        List<User> listWithExpectedUser = Collections.singletonList(user);
+        when(mockUserDAO.getObjects(properEntry)).thenReturn(listWithExpectedUser);
         User actualUser = loginService.getUser(login);
         User expectedUser = user;
         assertEquals(expectedUser, actualUser);
@@ -74,23 +61,21 @@ class LoginServiceTest {
 
     @Test
     public void testShouldReturnTrue_WhenUserIsValid() {
-//        when(user.getPassword()).thenReturn(password);
+        List<User> listWithExpectedUser = Collections.singletonList(user);
+        when(mockUserDAO.getObjects(properEntry)).thenReturn(listWithExpectedUser);
         assertTrue(loginService.checkUser(user.getLogin(), user.getPassword()));
     }
 
-    //TODO in progress ,dolozyc casea z roznymi Entry przy tej samej metodzie (mockUserDao.getobjects), dla ponizszych 2 testow kiedy podane beda niewlasciwe dane
     @Test
     public void testShouldReturnFalse_WhenUserIsAbsent() {
-//        when(mockUser.getPassword()).thenReturn(password);
-//        when(user.getLogin()).thenReturn(login);
-//        when(mockUserDAO.getObjects(invalidEntry)).thenReturn(Collections.emptyList());
-        assertFalse(loginService.checkUser(login, password));
+        when(mockUserDAO.getObjects(invalidEntry)).thenReturn(Collections.emptyList());
+        assertFalse(loginService.checkUser("invalidLogin", password));
     }
 
     @Test
     public void testShouldReturnFalse_WhenPasswordIsIncorrect() {
-//        when(user.getPassword()).thenReturn(password);
-//        when(mockUser.getLogin()).thenReturn(login);
+        List<User> listWithExpectedUser = Collections.singletonList(user);
+        when(mockUserDAO.getObjects(properEntry)).thenReturn(listWithExpectedUser);
         assertFalse(loginService.checkUser(login, "incorrectPassword"));
     }
 }
