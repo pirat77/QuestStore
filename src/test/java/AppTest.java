8089b464fc7b6c9a03fb1922c15c06a9053cde8e
MateuszGolda <mateusz.golda1993@gmail.com>
2020-09-03 18:19:21 +0200

    test: add classes which should be given as arguments to handle context

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,5 +1,8 @@
 import com.sun.net.httpserver.HttpServer;
 import handler.CookieHandler;
+import handler.FileHandler;
+import handler.LoginHandler;
+import handler.student.StudentPagesHandler;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -21,15 +24,15 @@ class AppTest {
     void should_ExecuteServerMethods_WhenAppSetPathsCalled() {
         App.setPaths(cookieHandler, server);
         assertAll(
-                () -> verify(server, times(1)).createContext(eq("/static"), any()),
-                () -> verify(server, times(1)).createContext(eq("/images"), any()),
-                () -> verify(server, times(1)).createContext(eq("/login"), any()),
-                () -> verify(server, times(1)).createContext(eq("/student/artifact"), any()),
-                () -> verify(server, times(1)).createContext(eq("/student/home"), any()),
-                () -> verify(server, times(1)).createContext(eq("/student/quest"), any()),
-                () -> verify(server, times(1)).createContext(eq("/student/raid"), any()),
-                () -> verify(server, times(1)).createContext(eq("/student/student"), any()),
-                () -> verify(server, times(1)).createContext(eq("/student/store"), any()),
+                () -> verify(server, times(1)).createContext(eq("/static"), any(FileHandler.class)),
+                () -> verify(server, times(1)).createContext(eq("/images"), any(FileHandler.class)),
+                () -> verify(server, times(1)).createContext(eq("/login"), any(LoginHandler.class)),
+                () -> verify(server, times(1)).createContext(eq("/student/artifact"), any(StudentPagesHandler.class)),
+                () -> verify(server, times(1)).createContext(eq("/student/home"), any(StudentPagesHandler.class)),
+                () -> verify(server, times(1)).createContext(eq("/student/quest"), any(StudentPagesHandler.class)),
+                () -> verify(server, times(1)).createContext(eq("/student/raid"), any(StudentPagesHandler.class)),
+                () -> verify(server, times(1)).createContext(eq("/student/student"), any(StudentPagesHandler.class)),
+                () -> verify(server, times(1)).createContext(eq("/student/store"), any(StudentPagesHandler.class)),
                 () -> verify(server, times(1)).setExecutor(any()),
                 () -> verify(server, times(1)).start()
         );
